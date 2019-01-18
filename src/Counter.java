49f7683143d8d397ef97b4e16d5ebcdd272416cc
Mansukh <posixnnja@gmail.com>
2019-01-18 08:09:57 -0500

    1) Offset by 1. Bug fix 2) Write test cases 3) Documentation and comments

--- src/Counter.java
+++ src/Counter.java
@@ -22,7 +22,8 @@ public class Counter implements FibonacciCounter {
 
   @Override
   public int curPos() {
-    int x = curPosNum(pos-1);
+//    int x = curPosNum(pos-1);
+//    System.out.println(x+"Fib");
     return pos;
   }
 
@@ -35,15 +36,10 @@ public class Counter implements FibonacciCounter {
   }
 
   public static void main(String[] args) {
-    Counter fb = new Counter(4);
-//    System.out.println(fb.curPosNum(7));
-//    System.out.println(fb.curPos(8));
+    Counter fb = new Counter(0);
 
-    //fb.decCount();
-    //fb.curPos()
+    //fb.incCount();
     System.out.println(fb.curPos());
-    //System.out.println(fb.curPosNum(8));
-    //System.out.println();
-    System.out.print(fb.incCount());
+    System.out.println(fb.curPosNum(fb.curPos())+":Fibo");
   }
 }
